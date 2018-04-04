dabb754ea4fc16039d4c4a12fe5655b08fd39d59
MIKEEEKIM <33226384+MIKEEEKIM@users.noreply.github.com>
2018-04-04 19:31:48 +0800

    day 1

---
+++ CodeFile2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace fuckapplication
+{
+
+    class Suanmianji
+    {
+
+        double chang;
+        double kuan;
+        public void Acceptdetails()
+        {
+            chang = 4;
+
+                kuan = 3;
+
+
+
+
+        }
+
+        public double GetArea()
+
+        {
+
+            return chang * kuan;
+
+
+        }
+
+
+
+
+
+        public void Display()
+        {
+
+            Console.WriteLine("chang:{0}", chang);
+            Console.WriteLine("kuan:{0}", kuan);
+            Console.WriteLine("mianji:{0}", GetArea());
+
+        }
+
+
+    }
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+
+            Suanmianji r = new Suanmianji ();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace HelloworldApplication
+{
+
+class Helloworld
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("nihaoshijie");
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+        }
+        
+        
+
+
+
+}
