8b9366b616f87dee21eaf176840517c06c0f7ee3
MIKEEEKIM <33226384+MIKEEEKIM@users.noreply.github.com>
2018-04-05 21:50:51 +0800

    继续coding
    今天也要加油

---
+++ codeeee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using suan;
+
+namespace Helloword
+{
+
+
+
+    class Program
+
+
+
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            double res = Suan.Sub(128, 1288);
+            Console.WriteLine(res);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
