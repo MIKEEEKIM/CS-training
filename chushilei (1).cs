6b4132d9a83c7690215943c76de99a5a93419036
MIKEEEKIM <33226384+MIKEEEKIM@users.noreply.github.com>
2018-04-06 17:09:45 +0800

    关于类
    感觉会了一点

---
+++ chushilei (1).cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+
+namespace Renshilei
+{
+
+    class Programme
+    {
+
+
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+
+            form.Text="dajiahaowshizhazhahui";
+            form.ShowDialog();
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
