a98c6d0c1b46a6f4dbb8ffa46925e9bdfa998c77
MIKEEEKIM <33226384+MIKEEEKIM@users.noreply.github.com>
2018-04-09 22:58:00 +0800

    加油
    接触方法

---
+++ CodeFile6.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace LiainXi
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+            float x = 3.0F;
+            Console.WriteLine(x.GetType().Name);
+            Calculator c = new Calculator();
+            int y = c.Add(2, 3);
+            Console.WriteLine(y);
+            c.PrintSum(4, 6);
+
+        }
+
+
+
+
+
+
+
+    }
+
+    class Calculator
+    {
+        public int Add(int a, int b)
+
+        {
+
+            int result = a + b;
+            return result;
+
+
+        }
+
+
+
+
+
+
+
+        public void PrintSum(int a, int b)
+        {
+
+            int reslit = a + b;
+            Console.WriteLine(reslit);
+
+
+        }
+
+    }
+
+
+
+
+    
+
+
+
+}
